60a703ed8dcc8bea6f74337e6eef3821ba44527f
Theonlysmartboy <o2jose43@gmail.com>
2023-09-22 11:28:15 +0300

    Begun adding authentication module

---
+++ app/src/main/java/com/otemainc/ngugehillslodgepos/ui/auth/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.otemainc.ngugehillslodgepos.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.otemainc.ngugehillslodgepos.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/otemainc/ngugehillslodgepos/ui/auth/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.otemainc.ngugehillslodgepos.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.otemainc.ngugehillslodgepos.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
